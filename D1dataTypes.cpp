73c7cb3d61b67a25887a9350e9e59eb9a9858698
Akash <akashrajpots14@gmail.com>
2021-05-11 12:32:08 +0530

    UPdate ist file of day 1

---
+++ D1dataTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   short int a = 10;
+     float b = 10.2;
+     char c = 'a';
+    double d =1.23;
+
+
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(b)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(d)<<endl; 
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+
+    char c = 'a';
+    double d ='1.23';
+
+
+    cout<<sizeof(a);
+    cout<<sizeof(b);
+    cout<<sizeof(c);
+    cout<<sizeof(d); 
+}
