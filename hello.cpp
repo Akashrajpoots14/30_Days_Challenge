e4e5bb06578e0622fc70f6ae050966719a4ac48f
Akash <akashrajpots14@gmail.com>
2021-05-13 22:14:07 +0530

    HEllo file added

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 int main(){
+
+    cout<<"HEllo";
+ 
     int n;
     cin>>n;
-    cout<<n;
-    return 0;
+    int m;
+    cin>>m;
+
+
+    cout<<n+m;
+       return 0;
 }
